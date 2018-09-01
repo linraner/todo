0116d38c81b3359d048fbf83c80e013961ddbad4
Frank <linran@live.cn>
2018-09-01 20:11:30 +0800

    -

--- js/base.js
+++ js/base.js
@@ -1,3 +1,13 @@
 ;(function () {
     'use strict';
+
+    var $form_add_task = $('.add-task')
+        , new_task = {}
+    ;
+
+    $form_add_task.on('submit', function (e) {
+        e.preventDefault();
+        new_task.content = $(this).find('input[name=content]').val();
+        console.log('new_task', new_task);
+    })
 })();
